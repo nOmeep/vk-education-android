b7dd12ffa8e0a94cc920624b052bc66b4188b6a5
Анна Терентьева <annyshka20072002@gmail.com>
2022-04-13 22:12:04 +0300

    add-fragment-of-lists

---
+++ app/src/main/java/com/example/viewed/ui/fragments/lists/ListsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.viewed.ui.fragments.lists
+
+import com.example.viewed.databinding.FragmentListsBinding
+import com.example.viewed.ui.fragments.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ListsFragment : BaseFragment<FragmentListsBinding>(FragmentListsBinding::inflate)
--- app/src/main/java/com/example/viewed/ui/fragments/profile/ProfileFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.example.viewed.ui.fragments.profile
-
-import com.example.viewed.databinding.FragmentProfileBinding
-import com.example.viewed.ui.fragments.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate)
