7ff27d4297d83995eea11dca7d306ba74f2698e5
aleksandr <aleksandrnaumenko@mail.ru>
2022-04-20 19:31:02 +0300

    fix-BaseFragment

--- app/src/main/java/com/example/viewed/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/viewed/ui/fragments/profile/ProfileFragment.kt
@@ -2,24 +2,18 @@ package com.example.viewed.ui.fragments.profile
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.viewed.R
 import com.example.viewed.databinding.FragmentProfileBinding
+import com.example.viewed.ui.fragments.BaseFragment
 import com.example.viewed.ui.fragments.viewmodels.ProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ProfileFragment : Fragment(R.layout.fragment_profile) {
-    private var _binding: FragmentProfileBinding? = null
-    private val binding
-        get() = _binding!!
-
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
     private val viewModel by viewModels<ProfileViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentProfileBinding.bind(view)
 
         val profileMoviesAdapter = ProfileItemsAdapter { x: Int -> viewModel.delMoviesByViewed(x) }
 
@@ -43,10 +37,5 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
             profileMoviesAdapter.setDelete { x: Int -> viewModel.delMoviesByWatch(x) }
             viewModel.switchMoviesWatch()
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
