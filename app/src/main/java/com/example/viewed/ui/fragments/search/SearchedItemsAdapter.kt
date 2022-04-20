f6e3a3ecea8fc60858e02e0b6263f3e87c19d869
Анна Терентьева <annyshka20072002@gmail.com>
2022-04-20 19:21:53 +0300

    rounded-corner-for-img

--- app/src/main/java/com/example/viewed/ui/fragments/search/SearchedItemsAdapter.kt
+++ app/src/main/java/com/example/viewed/ui/fragments/search/SearchedItemsAdapter.kt
@@ -6,6 +6,8 @@ import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.example.viewed.R
 import com.example.viewed.api.items.MoviePage.Info
 import com.example.viewed.databinding.ItemSearchResultMovieBinding
@@ -19,13 +21,14 @@ class SearchedItemsAdapter : PagingDataAdapter<Info, SearchedItemsViewHolder>(DI
             binding.apply {
                 Glide.with(itemView)
                     .load("https://image.tmdb.org/t/p/w500/${info.poster_path}")
+                    .apply (RequestOptions.bitmapTransform (RoundedCorners (14)))
                     .error(R.drawable.no_poster)
                     .into(ivMoviePoster)
 
                 tvTitle.text = info.title
                 val data: String = info.release_date
                 val year: String = if (data.length > 4) data.substring(0, 4) else data
-                tvYearCountry.text = year
+                tvYear.text = year
                 tvDescription.text = info.overview
                 tvRating.text = info.vote_average.toString()
             }
